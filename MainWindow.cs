e7f872e68b85c62c772c0566bd947cc62d89017a
juanr <juanr@MEGU>
2023-11-06 22:47:21 +0800

    Add project files.

---
+++ MainWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marine_Adventures
+{
+    internal class MainWindow : Form
+    {
+        private double WINDOW_HEIGHT, WINDOW_WIDTH;
+        private Button start = new Button();
+        private Button load = new Button();
+        private Button highscore = new Button();
+        private Button exit = new Button();
+
+        public MainWindow()
+        {
+            WINDOW_HEIGHT = Screen.PrimaryScreen.Bounds.Height / 1.2;
+            WINDOW_WIDTH = Screen.PrimaryScreen.Bounds.Width / 1.2;
+            this.Text = "Marine Adventures";
+            this.Size = new Size((int)WINDOW_WIDTH, (int)WINDOW_HEIGHT);
+
+            System.Windows.Forms.Button[] buttons = { start, load, highscore, exit };
+
+            start.Text = "Start";
+            load.Text = "Load";
+            highscore.Text = "High score";
+            exit.Text = "Exit";
+
+            start.Left = (this.ClientSize.Width - start.Width) / 2;
+            start.Top = (this.ClientSize.Height - start.Height) / 2;
+            start.Anchor = AnchorStyles.None;
+
+            // TODO: MAKE INTO FOR LOOP
+            load.Location = new Point(start.Left, start.Height + start.Top + 10);
+            load.Anchor = AnchorStyles.None;
+            highscore.Location = new Point(load.Left, load.Height + load.Top + 10);
+            highscore.Anchor = AnchorStyles.None;
+            exit.Location = new Point(highscore.Left, highscore.Height + highscore.Top + 10);
+            exit.Anchor = AnchorStyles.None;
+
+            exit.Click += new EventHandler(ExitButtonEvent);
+
+            foreach (System.Windows.Forms.Button button in buttons)
+            {
+                this.Controls.Add(button);
+            }
+        }
+
+        private void ExitButtonEvent(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marine_Adventures
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MainWindow mainWindow = new MainWindow();
+            Application.Run(mainWindow);
+        }
+    }
+}
