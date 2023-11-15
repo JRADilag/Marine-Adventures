95cc493228ecda70633771a7965eb8d4ccaef4ae
Chalesgo <150835564+Chalesgo@users.noreply.github.com>
2023-11-15 21:10:59 +0800

    Update HighscoreScreen.cs

--- HighscoreScreen.cs
+++ HighscoreScreen.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Marine_Adventures
 {
@@ -18,8 +19,22 @@ namespace Marine_Adventures
             string playerName, playerScore;
             string[] playerData = new string[2];
             string highScore;
-            highScores.Size = new System.Drawing.Size(this.Width, this.Height);
+
+            this.Size = new Size(500, 400);
+            this.Text = "High Scores";
+
+            Button backButton = new Button();
+            backButton.Text = "Back";
+            backButton.Size = new Size(80, 30);
+            backButton.Location = new Point(10, 10);
+            backButton.Click += BackButton_Click;
+            this.Controls.Add(backButton);
+
+            highScores.Location = new Point(10, 50);
+            UpdateListBoxSize(); 
+
             this.Controls.Add(highScores);
+
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -35,9 +50,26 @@ namespace Marine_Adventures
                 highScore = "Name: " + playerData[0] + "\tScore: " + playerData[1];
 
                 highScores.Items.Add(highScore);
-
-                highScores.EndUpdate();
             }
+
+            highScores.EndUpdate();
+
+            this.Resize += HighscoreScreen_Resize;
+        }
+
+        private void HighscoreScreen_Resize(object sender, EventArgs e)
+        {
+            UpdateListBoxSize();
+        }
+
+        private void UpdateListBoxSize()
+        {
+            highScores.Size = new Size(this.ClientSize.Width - 20, this.ClientSize.Height - 70);
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
